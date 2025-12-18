dde5ed4737842f9bc53117dd335488a7ea6f66e5
bala-amypo <bala@amypo.in>
2025-12-18 12:48:31 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements ServletContextInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    /**
+     * Required so that @WebServlet(SimpleStatusServlet) is detected
+     * during startup and by automated test suites.
+     */
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // no-op
     }
 }
