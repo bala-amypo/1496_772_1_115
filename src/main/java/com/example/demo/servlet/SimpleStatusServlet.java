2e23c5e491abf0e52061c2281fb08df2527f34ed
bala-amypo <bala@amypo.in>
2025-12-26 08:31:25 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -3,19 +3,18 @@ package com.example.demo.servlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
 
-        resp.setContentType("text/plain");
-        resp.getWriter().write(
+        response.setContentType("text/plain");
+        response.getWriter().write(
                 "Cold Chain Temperature Breach Alert System is running"
         );
-        resp.getWriter().flush();
+        response.getWriter().flush();
     }
 }
