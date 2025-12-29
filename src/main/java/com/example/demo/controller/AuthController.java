3fd781d6eefafd38f75d4b0e71ddc8375d5f4278
bala-amypo <bala@amypo.in>
2025-12-29 09:20:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -42,14 +42,16 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<LoginResponse> register(@RequestBody RegisterRequest req) {
+    public ResponseEntity<String> register(@RequestBody RegisterRequest req) {
 
-        User saved = userService.registerUser(
-            new User(null, req.getFullName(), req.getEmail(), req.getPassword(), "MONITOR")
-        );
-       
-        String token = jwtUtil.generateToken(saved.getId(), saved.getEmail(), saved.getRole());
-     
-        return ResponseEntity.ok(new LoginResponse(token));
-    }
+    User saved = userService.registerUser(
+        new User(null, req.getFullName(), req.getEmail(), req.getPassword(), "MONITOR")
+    );
+
+    emailService.sendWelcomeEmail(saved.getEmail(), saved.getFullName());
+
+    return ResponseEntity.ok("Registration successful. Email sent to user.");
 }
+
+    }
+
