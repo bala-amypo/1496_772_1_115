9d3bad41f2713b341e57b46410e91222ab216ca2
bala-amypo <bala@amypo.in>
2025-12-29 09:32:19 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginResponse.java
+++ src/main/java/com/example/demo/dto/LoginResponse.java
@@ -1,13 +1,20 @@
 package com.example.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class LoginResponse {
+
+    @JsonIgnore
     private String token;
+
+    private String message;
 
     public LoginResponse(String token) {
         this.token = token;
+        this.message = "Registration successful";
     }
 
-    public String getToken() {
-        return token;
+    public String getMessage() {
+        return message;
     }
 }
